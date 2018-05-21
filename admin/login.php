d287db0d43b156bbab7083d9efc478660a5722f3
sanhphanvan96 <sanhphanvan96@gmail.com>
2018-05-21 17:02:08 +0700

    add admin page

---
+++ admin/login.php
@@ -0,0 +1,47 @@
+<!-- header -->
+<?php include_once 'inc/header.php'; ?>
+<!-- /.header -->
+
+	<div class="page-content container">
+		<div class="row">
+			<div class="col-md-4 col-md-offset-4">
+				<div class="login-wrapper">
+			        <div class="box">
+			            <div class="content-wrap">
+			            	<img width="100px" height="100px" class="img-circle" src="images/icon-180x180.png">
+			                <h6>Đăng nhập</h6>
+			                
+			                <div class="form-group">
+			                	<label class="text-left pull-left" for="username">Tên đăng nhập</label>
+			               		<input class="form-control" type="text" placeholder="Username">
+			                </div>
+			                
+			                <div class="form-group">
+			                	<label class="text-left pull-left" for="password">Mật khẩu</label>
+			                	<input class="form-control" type="password" placeholder="Password">
+			                </div>
+			                
+			                <div class="action">
+			                    <a class="btn btn-primary signup btn-block" href="index.html">Login</a>
+			                </div>                
+			            </div>
+			        </div>
+
+			        <div class="already">
+			            <p>Don't have an account yet?</p>
+			            <a href="javascript:void(0)">Sign Up</a>
+			        </div>
+			    </div>
+			</div>
+		</div>
+	</div>
+
+
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://code.jquery.com/jquery.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+    <script src="js/custom.js"></script>
+  </body>
+</html>
