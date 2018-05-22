e1493b4b983d7bda36a8c83d166da943eaa0b33d
sanhphanvan96 <sanhphanvan96@gmail.com>
2018-05-22 11:44:29 +0700

    Can add faculties

--- admin/faculties.php
+++ admin/faculties.php
@@ -41,96 +41,48 @@
 						<thead>
 							<tr>
 								<th>ID</th>
-								<th>Tên</th>
-								<th>Danh mục</th>
-								<th>Lượt đọc</th>
-								<th>Hình ảnh</th>
+								<th>Tên khoa</th>
+								<th>Giới thiệu</th>
 								<th>Chức năng</th>
 							</tr>
 						</thead>
 						<tbody>
-							<tr class="odd gradeX">
-								<td>1</td>
-								<td>Internet
-									 Explorer 4.0</td>
-								<td>Win 95+</td>
-								<td class="center"> 4</td>
-								<td class="center text-center">
-									<img src="images/fff.png" />
-								</td>
-								<td class="center text-center">
-									<a href="" title="" class="btn btn-primary"><span class="glyphicon glyphicon-pencil "></span> Sửa</a>
-                                    <a href="" title="" class="btn btn-danger"><span class="glyphicon glyphicon-trash"></span> Xóa</a>
-								</td>
-							</tr>
-							<tr class="even gradeC">
-								<td>2</td>
-								<td>Internet
-									 Explorer 5.0</td>
-								<td>Win 95+</td>
-								<td>95</td>
-								<td class="center text-center">
-									<img src="images/fff.png" />
-								</td>
-								<td class="center text-center">
-									<a href="" title="" class="btn btn-primary"><span class="glyphicon glyphicon-pencil "></span> Sửa</a>
-                                    <a href="" title="" class="btn btn-danger"><span class="glyphicon glyphicon-trash"></span> Xóa</a>
-								</td>
-							</tr>
-							<tr class="odd gradeA">
-								<td>3</td>
-								<td>Internet
-									 Explorer 5.5</td>
-								<td>Win 95+</td>
-								<td class="center">5.5</td>
-								<td class="center text-center">
-									<img src="images/fff.png" />
-								</td>
-								<td class="center text-center">
-									<a href="" title="" class="btn btn-primary"><span class="glyphicon glyphicon-pencil"></span> Sửa</a>
-                                    <a href="" title="" class="btn btn-danger"><span class="glyphicon glyphicon-trash"></span> Xóa</a>
-								</td>
-							</tr>
-							<tr class="even gradeA">
-								<td>4</td>
-								<td>Internet
-									 Explorer 6</td>
-								<td>Win 98+</td>
-								<td class="center">6</td>
-								<td class="center text-center">
-									<img src="images/fff.png" />
-								</td>
-								<td class="center text-center">
-									<a href="" title="" class="btn btn-primary"><span class="glyphicon glyphicon-pencil "></span> Sửa</a>
-                                    <a href="" title="" class="btn btn-danger"><span class="glyphicon glyphicon-trash"></span> Xóa</a>
-								</td>
-							</tr>
-							<tr class="odd gradeA">
-								<td>5</td>
-								<td>Internet Explorer 7</td>
-								<td>Win XP SP2+</td>
-								<td class="center">7</td>
-								<td class="center text-center">
-									<img src="images/fff.png" />
-								</td>
-								<td class="center text-center">
-									<a href="" title="" class="btn btn-primary"><span class="glyphicon glyphicon-pencil "></span> Sửa</a>
-                                    <a href="" title="" class="btn btn-danger"><span class="glyphicon glyphicon-trash"></span> Xóa</a>
-								</td>
-							</tr>
-							<tr class="even gradeA">
-								<td>6</td>
-								<td>AOL browser (AOL desktop)</td>
-								<td>Win XP</td>
-								<td class="center">6</td>
-								<td class="center text-center">
-									<img src="images/fff.png" />
-								</td>
-								<td class="center text-center">
-									<a href="" title="" class="btn btn-primary"><span class="glyphicon glyphicon-pencil "></span> Sửa</a>
-                                    <a href="" title="" class="btn btn-danger"><span class="glyphicon glyphicon-trash"></span> Xóa</a>
-								</td>
-							</tr>
+						<?php
+							require_once "../utils/DBConnector.php";
+						?>
+						<?php
+							$statement = "SELECT * FROM `faculty` ORDER BY `name` ASC";
+							$res = $connect->query($statement);
+						
+							if($res) {
+								while ($row = $res->fetch_object()){
+							?>
+								<tr class="<?php if($row->id % 2 == 0) echo "odd";  else echo "even"; ?>">
+									<td width="150">
+										<?php echo "<code>".$row->idk."</code>"; ?>
+									</td>
+									<td width="200"><a href="../public/view.php?idk=<?php echo $row->idk; ?>" target="_blank">
+										<?php echo $row->name; ?></a>
+									</td>
+									<?php 
+									$info = $row->info; 
+									if (strlen($info) > 200)
+										$info = substr($info, 0, 200) . '...';
+									?>
+									<td width="500" height="100"><?php echo htmlspecialchars($info); ?></td>
+									<td width="200" class="center text-center">
+										<a href="facultyEdit.php?id=<?php echo $row->idk;?>" title="" class="btn btn-primary"><span class="glyphicon glyphicon-pencil "></span> Sửa</a>
+										<a href="facultyDel.php?id=<?php echo $row->idk;?>" 
+											title="" 
+											class="btn btn-danger"
+											onclick="return confirm('Bạn chắc chắn muốn xóa?');"
+											><span class="glyphicon glyphicon-trash"></span> Xóa</a>
+									</td>
+								</tr>
+							<?php 
+								} 
+							}
+						?>
 						</tbody>
 					</table>
 
---
+++ admin/facultyAdd.php
@@ -0,0 +1,93 @@
+<!-- header -->
+<?php include_once 'inc/header.php'; ?>
+<?php include_once '../utils/DBConnector.php'; ?>
+<!-- /.header -->
+
+<?php
+    if(isset($_POST["submit"])) {
+        // echo "<pre>";
+        // print_r($_REQUEST);
+        // echo "</pre>";
+        $idk = $_POST["idk"];
+        $name = $_POST["name"];
+		$info = $_POST["info"];
+        if($idk == "" || $name == "" || $info == "") {
+			header("location: facultyAdd.php");
+			die();
+		}
+		$sql = "INSERT INTO faculty (idk, name, info) VALUES 
+			('$idk', '$name', '$info')";
+		if ($connect->query($sql) === TRUE) {
+			// setFlash("success", "Đã tạo mới khoa thành công!");
+			echo "Đã tạo mới khoa thành công!";
+		} else {
+			// setFlash("error", "Tạo mới khoa không thành công! ".$connect->error);
+			echo "Tạo mới khoa không thành công! ".$connect->error;
+		}
+    }
+
+?>
+<form method="post" action="">
+    <div class="page-content">
+    	<div class="row">
+		  <div class="col-md-2">
+		  	<div class="sidebar content-box" style="display: block;">
+                <!-- Nav-bar -->
+				<?php include_once 'inc/left-bar.php'; ?>
+				<!-- /.nav-bar -->
+             </div>
+		  </div>
+		  <div class="col-md-10">
+
+	  			<div class="row">
+	  				<div class="col-md-12 panel-info">
+			  			<div class="content-box-header panel-heading">
+		  					<div class="panel-title ">Thêm thông báo</div>
+			  			</div>
+			  			<div class="content-box-large box-with-header">
+				  			<div>
+								<div class="row mb-10"></div>
+								
+								<div class="row">
+									<div class="col-sm-6">
+										<div class="form-group">
+											<label for="idk">Mã khoa</label>
+											<input name="idk" type="text" class="form-control" placeholder="KhoaCNTT">
+										</div>
+										<div class="form-group">
+											<label for="name">Tên khoa</label>
+											<input name="name" type="text" class="form-control" placeholder="Khoa Công nghệ Thông tin">
+										</div>
+									</div>
+
+									<div class="col-sm-6"></div>
+
+									<div class="col-sm-12">
+										<div class="form-group">
+										   <label>Giới thiệu</label>
+										   <textarea name="info" class="ckeditor form-control" rows="7"></textarea>
+										</div>
+									</div>
+								</div>
+
+								<hr>
+
+								<div class="row">
+									<div class="col-sm-12">
+										<input name="submit" type="submit" value="Thêm" class="btn btn-success" />
+										<input type="reset" value="Nhập lại" class="btn btn-default" />
+									</div>
+								</div>
+
+							</div>
+						</div>
+			  		</div>
+	  			</div><!-- /.row col-size -->
+	  		
+		  </div><!-- /.col-md-10 -->
+		</div><!-- /.row -->
+    </div><!-- Page content -->
+</form>
+    <script src="ckeditor/ckeditor.js"></script>
+   <!-- footer -->
+   <?php include_once 'inc/footer.php'; ?>
